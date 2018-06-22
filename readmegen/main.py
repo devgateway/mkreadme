653def7a6099caa1a02c0cef0e084b2437cc968a
Styopa Semenukha <ssemenukha@developmentgateway.org>
2018-06-22 15:45:15 -0400

    Set logging and parse args

--- readmegen/main.py
+++ readmegen/main.py
@@ -1,5 +1,48 @@
 # Copyright 2018, Development Gateway, Inc.
 # This file is part of readmegen, see COPYING.
 
+import logging, sys, os, argparse
+
+log = None
+
+def _set_log_level():
+    valid_levels = ['CRITICAL', 'ERROR', 'WARNING', 'INFO', 'DEBUG']
+    try:
+        env_level = os.environ['LOG_LEVEL']
+        valid_levels.remove(env_level)
+        level = getattr(logging, env_level)
+    except KeyError:
+        level = logging.WARNING
+    except ValueError:
+        msg = 'Expected log level: %s, got: %s. Using default level WARNING.' \
+                % ('|'.join(valid_levels), env_level)
+        print(msg, file = sys.stderr)
+        level = logging.WARNING
+
+    logging.basicConfig(level = level)
+    global log
+    log = logging.getLogger(__name__)
+
 def main():
-    pass
+    ap = argparse.ArgumentParser(description = 'Generate Readme for Ansible roles')
+
+    ap.add_argument('--playbook', '-p',
+            type = argparse.FileType('r'),
+            help = 'A minimal playbook for this role')
+
+    ap.add_argument('--roledir', '-r',
+            default = '.',
+            help = 'Path to the role; default: current directory')
+
+    args = ap.parse_args()
+
+    try:
+        pass
+    except Exception as e:
+        if log.isEnabledFor(logging.DEBUG):
+            raise RuntimeError() from e
+        else:
+            sys.exit(str(e))
+
+if __name__ == '__main__':
+    main()
